c703e61126c12ffc6df74464f68fc8a0e5f47135
Metleb1996 <metlebrustemov1996@gmail.com>
2021-08-28 00:26:08 +0400

    Navbar tekmillesdirildi

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import os
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db_path = os.path.join(os.path.dirname(__file__), 'vt.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route("/")
 def index():
