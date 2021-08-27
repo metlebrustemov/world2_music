eb0183d752be903978bcb4e64aa8da05300df93c
Metleb1996 <metlebrustemov1996@gmail.com>
2021-08-27 23:30:43 +0400

    User modeli elave edildi

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def reg_user():
+    return render_template("register.html")
+
+@app.route("/login")
+def login_user():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
