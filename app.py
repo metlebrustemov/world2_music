a3c57f861a0101405e258852d4d8f9b819510a7d
Metleb1996 <metlebrustemov1996@gmail.com>
2021-08-28 22:13:38 +0400

    WTForms elave edildi

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms.fields.html5 import EmailField
 
 app = Flask(__name__)
 db_path = os.path.join(os.path.dirname(__file__), 'vt.db')
@@ -28,17 +30,39 @@ class W2Media(db.Model):
     def __repr__(self):
         return '<W2Media %r>' % self.u_name
 
+#//////////////////////////////////////////////////////////////////////
+
+class RegistrationForm(Form):
+    username = StringField('User Name', [validators.DataRequired(), validators.Length(min=4, max=25)], render_kw={"class":"form-control"})
+    email = EmailField('Email address', [validators.DataRequired(), validators.Length(min=8, max=35), validators.Email(message=(u'That\'s not a valid email address.'))], render_kw={"class":"form-control"})
+    password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=10, max=35), validators.EqualTo('confirm', message='Passwords must match')], render_kw={"class":"form-control"})
+    confirm = PasswordField('Repeat Password', [validators.DataRequired()], render_kw={"class":"form-control"})
+    accept_tos = BooleanField('Check me out', [validators.DataRequired()], render_kw={"class":"form-check-input"})
+
+class LoginForm(Form):
+    email = EmailField('Email address', [validators.DataRequired(), validators.Length(min=8, max=35), validators.Email(message=(u'That\'s not a valid email address.'))], render_kw={"class":"form-control"})
+    password = PasswordField('Password', [validators.DataRequired()],validators.Length(min=10, max=35), render_kw={"class":"form-control"})
+    accept_tos = BooleanField('Check me out', [validators.DataRequired()], render_kw={"class":"form-check-input"})
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=['POST', 'GET'])
 def reg_user():
-    return render_template("register.html")
+    if request.method == 'POST' :
+        
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    return render_template("register.html", form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['POST', 'GET'])
 def login_user():
-    return render_template("login.html")
+    if request.method == 'POST' :
+        
+        return redirect(url_for('index'))
+    form = LoginForm()
+    return render_template("login.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
