e1da7f8eff0ce17780ad0b8a654856705e5aae2f
Metleb1996 <metlebrustemov1996@gmail.com>
2021-10-04 16:09:58 +0400

    Added 'successfully completed' message...

--- migrate.py
+++ migrate.py
@@ -8,3 +8,4 @@ if __name__ == "__main__":
     print("This operation deletes your existing database files and creates new ones. \nThis means that all your data will be deleted. \nDo you want to continue? (Y, n)")
     if (input("(Yes or No)>>>").lower() == "yes"):
         create_database()
+        print("Successfully completed!")
